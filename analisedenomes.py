923a4113b0e3e4055ba2a72b50a903c1bf96008a
Caique de Castro <106915103+Caiquw@users.noreply.github.com>
2025-01-15 13:15:34 -0300

    Delete analisedenomes.py

--- analisedenomes.py
+++
@@ -1,7 +0,0 @@
-nome = str(input("Insira seu nome completo\n")).strip()
-nomespl= nome.split()
-
-print(f"Nome em caixa-alta: {nome.upper()}")
-print(f"Nome em caixa-baixa: {nome.lower()}")
-print(f"Número de letras no nome: {len(nome) - nome.count(' ')}")
-print(f"Número de letras do primeiro nome: {len(nomespl[0])}")
